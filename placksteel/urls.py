77af56e64aeb542a1098fba898b0fd438510a3c9
OswaldoSoares <oswaldosoares@gmail.com>
2024-04-22 10:09:14 -0300

    Imported urls from website

--- placksteel/urls.py
+++ placksteel/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from website import urls as website_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
