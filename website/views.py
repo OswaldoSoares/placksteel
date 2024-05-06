a017d1832b4f2545bb6d3c82a8a6549b890d3c7a
OswaldoSoares <oswaldosoares@gmail.com>
2024-05-06 19:14:34 -0300

    Created view index_website

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_website(request):
+    context = {}
+    context.update()
+    return render(request, "website/index.html", context)
